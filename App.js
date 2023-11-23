97a2e751ee36fd293a7f19118786c04c8f20df9a
Shubham <ershyadav303@gmail.com>
2023-11-23 20:37:49 +0530

    started first commit

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Button, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        justifyContent: "space-around",
+        alignItems: "stretch",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+          flex: 1,
+          flexDirection: "row",
+          paddingTop: 20,
+          paddingBottom: 10,
+          justifyContent: "center",
+          alignItems: "centre",
+        }}
+      >
+        <TextInput
+          style={{
+            height: 40,
+            width: "70%",
+            margin: 10,
+
+            borderWidth: 1,
+            padding: 10,
+          }}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+        <Button title="Press me" />
+      </View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          flex: 8,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +63,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
